d05ec360bfdf87ad103ef93d6a92b90a8972903e
wanteng2018 <37799497+wanteng2018@users.noreply.github.com>
2018-12-05 15:45:17 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,26 +12,10 @@ int main()
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
     if (pcl::io::loadPLYFile<pcl::PointXYZRGB>("p1.ply", *cloud) == -1)
     {
-        PCL_ERROR("couldn't read file test_pcd.pcd \n");
+        PCL_ERROR("couldn't read file p1.ply \n");
         return (-1);
     }
-/*
-    // 创建点云  并设置适当的参数（width height is_dense）
-    cloud.width    = 5;
-    cloud.height   = 1;
-    cloud.is_dense = false;  //不是稠密型的
-    cloud.points.resize (cloud.width * cloud.height);  //点云总数大小
-     //用随机数的值填充PointCloud点云对象
-    for (size_t i = 0; i < cloud.points.size (); ++i)
-    {
-      cloud.points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
-      cloud.points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
-      cloud.points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
-    }
-
-    pcl::io::savePCDFileASCII ("test_pcd.pcd", cloud);
-*/
-    pcl::visualization::CloudViewer viewer("pcd viewer");
+    pcl::visualization::CloudViewer viewer("PLY viewer");
     viewer.showCloud(cloud);
     while (!viewer.wasStopped())
     {
